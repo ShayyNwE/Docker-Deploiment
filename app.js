4ef16b0886772503bacebb054332851ef82960df
Tom Julliat <tom.julliat@next-u.fr>
2026-03-05 14:54:02 +0100

    Fix: app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
-
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello Docker!' })
