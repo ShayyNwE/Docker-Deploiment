784b6c80d398e7de6d2d26709e6b5443ecb84c94
Tom Julliat <tom.julliat@next-u.fr>
2026-03-05 11:19:55 +0100

    Add CI pipeline

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 // app.js
 const express = require('express')
 const app = express()
+const port = 3000
 
+app.use(express.json())
+
+// GET endpoint
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello DevOps' })
+  res.json({ message: 'Hello DevOps!' })
 })
 
+// POST endpoint
+app.post('/data', (req, res) => {
+  const data = req.body
+  res.json({ message: 'Data received', data })
+})
+
+// Exporter app pour les tests
 module.exports = app
+
+// Lancer le serveur seulement si ce fichier est exécuté directement
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`API running on http://localhost:${port}`)
+  })
+}
--- app.test.js
+++ app.test.js
@@ -1,9 +1,8 @@
-// app.test.js
 const request = require('supertest')
-const app = require('./App')
+const app = require('./app')   
 
 test('GET / retourne un message', async () => {
   const res = await request(app).get('/')
   expect(res.status).toBe(200)
-  expect(res.body.message).toBe('Hello DevOps')
+  expect(res.body.message).toBe('Hello DevOps!') 
 })
