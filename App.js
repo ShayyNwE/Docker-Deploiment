613525a4e27618ee6ae923819fd04c6505739322
Tom Julliat <tom.julliat@next-u.fr>
2026-03-05 09:34:30 +0100

    Add Express API with GET and POST endpoints

---
+++ App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello DevOps!' });
+});
+
+app.post('/data', (req, res) => {
+  const data = req.body;
+  res.json({
+    message: 'Data received',
+    data: data
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API running on http://localhost:${port}`);
+});
